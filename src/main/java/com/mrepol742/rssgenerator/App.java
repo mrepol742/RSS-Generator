88693037898417a7376fb6cbd7af8a3274016b43
Melvin Jones Repol <mrepol742@gmail.com>
2023-12-29 10:08:57 +0800

    Initial Commit

--- src/main/java/com/mrepol742/rssgenerator/App.java
+++ src/main/java/com/mrepol742/rssgenerator/App.java
@@ -28,35 +28,51 @@ public class App {
     static SimpleDateFormat format =  new SimpleDateFormat("E, dd MMMM yyyy k:m:s z");
     static String header = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<rss xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns:content=\"http://purl.org/rss/1.0/modules/content/\" xmlns:atom=\"http://www.w3.org/2005/Atom\" version=\"2.0\" xmlns:media=\"http://search.yahoo.com/mrss/\">\n<channel>\n";
     static String footer = "</channel>\n</rss>";
-
     static StringBuilder rss = new StringBuilder();
     static Arg arg;
     static boolean isHome = false;
+    static long timeStarted = 0;
 
     public static void main(String[] args) throws IOException  {
+        timeStarted = System.currentTimeMillis();
         if (args.length == 0) {
             System.out.println("arguments:\n\t--domain [address]\n\t--publisher [name]\noptional:\n\t--projectFolder [location]");
             return;
         }
+
         arg = resolveArguments(args);
+
+        System.out.println("\n\nScanning for files...");
+        System.out.println("----------< " + arg.getDomain() + " >----------\nBuilding /Rss/index.xml\n");
+
         rss.append(header);
-                find(new File(arg.getProjectFolder()), arg.getDomain());
+        find(new File(arg.getProjectFolder()), arg.getDomain());
         for (Item item: items) {
-            rss.append(String.format("<item>\n" +
-    "  <title>%1$s</title>\n" +
-    "  <description>%2$s</description>\n" +
-    "  <link>%3$s</link>\n" + 
-    "  <creator>%4$s</creator>\n" +
-    "  <content medium=\"image\" url=\"%5$s\"/>\n", item.title, item.description, item.link, "<![CDATA[ " + arg.getPublisher() + " ]]>", item.medium_url));
-            rss.append("</item>\n");
+            rss.append(String.format("\t<item>\n" +
+    "\t\t<title>%1$s</title>\n" +
+    "\t\t<description>%2$s</description>\n" +
+    "\t\t<link>%3$s</link>\n" + 
+    "\t\t<creator>%4$s</creator>\n" +
+    "\t\t<content medium=\"image\" url=\"%5$s\"/>\n", item.title, item.description, item.link, "<![CDATA[" + arg.getPublisher() + "]]>", item.medium_url));
+            rss.append("\t</item>\n");
         }
         rss.append(footer);
         Files.createDirectories(Paths.get(arg.getProjectFolder() + "/rss"));
+        StringBuilder sb = new StringBuilder("\n------------------------------------------------------------------------");
         if (write(new File(arg.getProjectFolder() + "/rss/index.xml"), rss.toString(), false)) {
-            System.out.println("\nRSS generated for " + arg.getDomain());
+             sb.append("\nBuild Success");
         } else {
-            System.out.println("\nFailed to generate rss.");
+            sb.append("\nBuild Failed");
         }
+        sb.append("\n------------------------------------------------------------------------");
+        long sum = System.currentTimeMillis() - timeStarted;
+        sb.append("\nTotal time: ");
+        sb.append(String.valueOf(sum));
+        sb.append(" s");
+        sb.append("\nFinished at: ");
+        sb.append(format.format(System.currentTimeMillis()));
+        sb.append("\n------------------------------------------------------------------------");
+        System.out.println(sb.toString());
     }
 
     public static String getTitle(File file) {
@@ -91,37 +107,37 @@ public class App {
         }
         if (file.isDirectory() && !isHome) {
             String[] metas = getMeta(new File(arg.getProjectFolder() + "/index.html"));
-            rss.append("  <title>");
+            rss.append("\t<title>");
             rss.append("<![CDATA[");
             rss.append(getTitle(new File(arg.getProjectFolder() + "/index.html")));
             rss.append("]]>");
             rss.append("</title>\n");
-            rss.append("  <description>");
+            rss.append("\t<description>");
             rss.append("<![CDATA[");
-            rss.append( metas[0]);
+            rss.append(metas[0]);
             rss.append("]]>");
             rss.append("</description>\n");
-            rss.append("  <link>");
-            rss.append(arg.getDomain());
-            rss.append("  </link>\n");
-            rss.append("  <image>\n");
-            rss.append("    <url>");
-            rss.append(metas[1]);
-            rss.append("</url>\n");
-            rss.append("    <title>");
-            rss.append(getTitle(new File(arg.getProjectFolder() + "/index.html")));
-            rss.append("</title>\n");
-            rss.append("    <link>");
+            rss.append("\t<link>");
             rss.append(arg.getDomain());
             rss.append("</link>\n");
-            rss.append("  </image>\n  <generator> RSS for ");
+            rss.append("\t<image>\n");
+            rss.append("\t\t<url>");
+            rss.append(metas[1]);
+            rss.append("</url>\n");
+            rss.append("\t\t<title>");
+            rss.append(getTitle(new File(arg.getProjectFolder() + "/index.html")));
+            rss.append("</title>\n");
+            rss.append("\t\t<link>");
+            rss.append(arg.getDomain());
+            rss.append("</link>\n");
+            rss.append("\t</image>\n\t<generator><![CDATA[RSS for ");
             rss.append(arg.getPublisher());
-            rss.append(" </generator>\n");
-            rss.append("  <lastBuildDate>");
+            rss.append("]]></generator>\n");
+            rss.append("\t<lastBuildDate>");
             rss.append(format.format(new Date()));
             rss.append("</lastBuildDate>\n");
-            rss.append("  <link href=\"" + arg.getDomain() + "/rss/index.xml\"" + " rel=\"self\" type=\"application/rss+xml\"/>\n");
-            rss.append("  <language><![CDATA[ en ]]></language>\n");
+            rss.append("\t<link href=\"" + arg.getDomain() + "rss/index.xml\"" + " rel=\"self\" type=\"application/rss+xml\"/>\n");
+            rss.append("\t<language><![CDATA[en]]></language>\n");
             isHome = true;
         }
 
@@ -131,13 +147,15 @@ public class App {
             if (folder.isDirectory()) {
                 File hasIndex = new File(folder.getAbsolutePath() + "/index.html");
                 if (hasIndex.isFile()) {
-                    System.out.println(format.format(hasIndex.lastModified()) + " | " + (arg.getDomain() + hasIndex.getParentFile().getAbsolutePath().replace(arg.getProjectFolder(), "")).replace("//", "/"));
-                    String[] metas = getMeta(new File(hasIndex.getParentFile().getAbsolutePath() + "/index.html"));
-                     items.add(new Item("<![CDATA[" + getTitle(new File(hasIndex.getParentFile().getAbsolutePath() + "/index.html")) + "]]>", "<![CDATA[" + metas[0] + "]]>", (arg.getDomain() + hasIndex.getParentFile().getAbsolutePath().replace(arg.getProjectFolder(), "")).replace("//", "/"), metas[1]));
-                     find(new File (file.getAbsolutePath() + "/" + str), arg.getDomain());
+                    String lastMod = format.format(hasIndex.lastModified());
+                    String location = arg.getDomain() + hasIndex.getParentFile().getAbsolutePath().replace(arg.getProjectFolder() + "/", "");
+                    System.out.println(lastMod + " | " + location);
+                    File parentFile = new File(hasIndex.getParentFile().getAbsolutePath() + "/index.html");
+                    String[] metas = getMeta(parentFile);
+                    items.add(new Item("<![CDATA[" + getTitle(parentFile) + "]]>", "<![CDATA[" + metas[0] + "]]>", location, metas[1]));
+                    find(new File (file.getAbsolutePath() + "/" + str), arg.getDomain());
                 }
             }
-        
         }
     }
 
@@ -157,7 +175,7 @@ public class App {
         return false;
     }
 
-       public static String read(java.io.File fe, String line) {
+    public static String read(java.io.File fe, String line) {
         try {
             if (!fe.exists()) {
                 return null;
